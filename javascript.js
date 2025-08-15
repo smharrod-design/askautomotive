6d31ffa0949e8ddb9042ac630aa2d465705e5c8c
smharrod-design <smharrod@outlook.com>
2025-08-15 16:09:43 +0100

    updated to simply book widget and adjusted content accordingly

--- javascript.js
+++ javascript.js
@@ -62,3 +62,14 @@
     // Ensure desktop remains interactive via CSS; default mobile is non-interactive
     setInteractive(false);
   })();
+
+// Add SimplyBook widget script to the page
+    (function(w, d, s, i) {
+        var script = d.createElement(s);
+        script.async = true;
+        script.src = "//widget.simplybook.it/v2/widget/widget.js";
+        script.onload = function() {
+            new SimplybookWidget({"widget_type":"iframe","url":"https:\/\/askautomotive.simplybook.it","theme":"minimal","theme_settings":{"timeline_show_end_time":"1","timeline_modern_display":"as_slots","hide_company_label":"0","timeline_hide_unavailable":"1","hide_past_days":"0","sb_base_color":"#245d7b","btn_color_1":"#245d7b,#245d7b,#245d7b","link_color":"#245d7b","display_item_mode":"list","body_bg_color":"#e7eef3","sb_review_image":"3","sb_review_image_preview":"\/uploads\/askautomotive\/image_files\/preview\/b7cb9c91fee8bcef2a78d5ed2cb41288.png","dark_font_color":"#2b212b","light_font_color":"#ffffff","sb_company_label_color":"#e8e8e8","hide_img_mode":"0","sb_busy":"#c22727","sb_available":"#2b212b"},"timeline":"modern","datepicker":"top_calendar","is_rtl":false,"app_config":{"clear_session":1,"allow_switch_to_ada":0,"predefined":[]},"container_id":"sbw_0m3y0p"});
+        };
+        d.head.appendChild(script);
+    })(window, document, 'script', 'sbw_0m3y0p');
